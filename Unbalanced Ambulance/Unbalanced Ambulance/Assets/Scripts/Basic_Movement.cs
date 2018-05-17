c2841916dfc64e28c2bcb7701679436920385774
Connor Wood <cnrw98@gmail.com>
2018-05-16 20:50:42 -0400

    First Commit
    Whole (current) unity3D project
    basic movement for half of an ambulance with a swinging door.

---
+++ Unbalanced Ambulance/Unbalanced Ambulance/Assets/Scripts/Basic_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic_Movement : MonoBehaviour {
+
+	//! Create variables:
+	int speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 5;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//! Forwards and back:
+		if (Input.GetKey(KeyCode.W)){
+			transform.Translate(-speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.S)){
+			transform.Translate(speed * Time.deltaTime, 0, 0);
+		}
+		//! Strafing:
+		if (Input.GetKey(KeyCode.A)){
+			transform.Rotate(0, 0, -100 * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			transform.Rotate(0, 0, 100 * Time.deltaTime);
+		}
+	}
+}
